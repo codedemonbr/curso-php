289531d3037772d7db6948bec70078b480efe66a
Thiago Henrique dos Santos <henrique.thsantos.ths@gmail.com>
2020-11-02 11:53:40 -0300

    Adicionados Estilo e Cabeçalho

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$page = $_SERVER['PHP_SELF'];
+$sec = "60";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="refresh" content="<?php echo $sec ?>;URL='<?php echo $page ?>'">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Indice dos exercícios</h2>
+    </header>
+    <main class="principal">principal</main>
+    <footer class="rodape">rodapeee</footer>
+</body>
+
+</html>
